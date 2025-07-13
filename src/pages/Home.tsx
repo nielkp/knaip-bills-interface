b0636d50a12a610192863d5d7d480af77e30ca6e
Daniel Knaip <danielknaip@gmail.com>
2025-07-13 14:24:05 -0300

    inicio da tela Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,24 @@ import Button from '../components/Button';
 const Home = () => {
   return (
     <div>
-      <h2>Home - KnaipBills</h2>
-      <Button>Clique em mim</Button>
+      <div>
+        <section>
+          <div>
+            <div>
+              <h1>
+                Gerencie suas finanças com o <span>KnaipBills</span>
+              </h1>
+              <p>
+                Uma plataforma simples e eficiente para controlar suas despesas. Organize suas
+                finanças pessoais ou do seu negócio com facilidade.
+              </p>
+            </div>
+            <div>
+              <Button>Acessar</Button>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
