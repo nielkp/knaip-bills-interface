cb90f3a4fca3c44570a850a48855d03ab74ebe0e
Daniel Knaip <danielknaip@gmail.com>
2025-07-24 12:46:40 -0300

    começando a tela de Login

---
+++ src/pages/Login.tsx
@@ -0,0 +1,21 @@
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <header>
+          <h1>KnaipBills</h1>
+          <p>Gerencie suas finanças de forma simples e eficiente</p>
+        </header>
+
+        <main>
+          <section>
+            <h2>Faça login para continuar</h2>
+            <p>Acesse sua conta para começar a gerenciar suas finanças</p>
+          </section>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
