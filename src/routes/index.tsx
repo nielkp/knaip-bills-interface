7bb47d91657facf1195d3b14e88e6a13ee196abd
Daniel Knaip <danielknaip@gmail.com>
2025-07-25 12:47:10 -0300

    criado rota privada para usuarios autenticados e adicionado esboço da tela de dashboard!

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AuthProvider } from '../context/AuthContext';
+import AppLayout from '../layout/AppLayout';
+import Dashboard from '../pages/Dashboard';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import PrivateRoutes from './PrivateRoutes';
 
 const AppRoutes = () => {
   return (
@@ -10,6 +13,13 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+
+          <Route element={<PrivateRoutes />}>
+            <Route element={<AppLayout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Route>
+
           <Route path="*" element={<h1>Página não encontrada!</h1>} />
         </Routes>
       </AuthProvider>
